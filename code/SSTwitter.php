dea716e446d1aebe26d9734d894fbd572781a86e
helpfulrobot <chris@silverstripe.com>
2016-01-01 02:29:10 +1300

    Converted to PSR-2

--- code/SSTwitter.php
+++ code/SSTwitter.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once (dirname(dirname(__FILE__)) . '/vendor/codebird-php/src/codebird.php');
+require_once(dirname(dirname(__FILE__)) . '/vendor/codebird-php/src/codebird.php');
 
 /**
  * The SSTwitter class connects to Twitter API version 1.1 via OAuth
@@ -8,106 +8,114 @@ require_once (dirname(dirname(__FILE__)) . '/vendor/codebird-php/src/codebird.ph
  *
  * @package twitter-stripe
  */
-class SSTwitter extends Object {
+class SSTwitter extends Object
+{
 
-	private $cb;
+    private $cb;
 
-	private $consumerKey;
-	private $consumerSecret;
+    private $consumerKey;
+    private $consumerSecret;
 
-	private $accessToken;
-	private $accessSecret;
+    private $accessToken;
+    private $accessSecret;
 
-	private $screenName;
+    private $screenName;
 
-	public function __construct() {
-		// set OAuth details from config
-		$this->setAuth();
-		// connect and setup instance
-		$this->connect();
-	}
+    public function __construct()
+    {
+        // set OAuth details from config
+        $this->setAuth();
+        // connect and setup instance
+        $this->connect();
+    }
 
-	/**
-	 *
-	 * Setup the auth properties from the config
-	 *
-	 */
-	private function setAuth() {
-		//set the auth details
-		$this->consumerKey = Config::inst()->get('SSTwitter', 'CONSUMER_KEY');
-		$this->consumerSecret = Config::inst()->get('SSTwitter', 'CONSUMER_SECRET');
+    /**
+     *
+     * Setup the auth properties from the config
+     *
+     */
+    private function setAuth()
+    {
+        //set the auth details
+        $this->consumerKey = Config::inst()->get('SSTwitter', 'CONSUMER_KEY');
+        $this->consumerSecret = Config::inst()->get('SSTwitter', 'CONSUMER_SECRET');
 
-		$this->accessToken = Config::inst()->get('SSTwitter', 'OAUTH_TOKEN');
-		$this->accessSecret = Config::inst()->get('SSTwitter', 'OAUTH_SECRET');
-	}
+        $this->accessToken = Config::inst()->get('SSTwitter', 'OAUTH_TOKEN');
+        $this->accessSecret = Config::inst()->get('SSTwitter', 'OAUTH_SECRET');
+    }
 
-	/**
-	 *
-	 * Setup the screen name via a param or the config
-	 * The config can be overridden by providing a param
-	 *
-	 * @param string $screenName Screen Name for a user timeline
-	 */
-	private function setScreenName($screenName = null) {
-		if (!$screenName) {
-			$screenName = Config::inst()->get('SSTwitter', 'TWITTER_SCREENNAME');
-		}
-		$this->screenName = $screenName;
-	}
+    /**
+     *
+     * Setup the screen name via a param or the config
+     * The config can be overridden by providing a param
+     *
+     * @param string $screenName Screen Name for a user timeline
+     */
+    private function setScreenName($screenName = null)
+    {
+        if (!$screenName) {
+            $screenName = Config::inst()->get('SSTwitter', 'TWITTER_SCREENNAME');
+        }
+        $this->screenName = $screenName;
+    }
 
-	/**
-	 *
-	 * Connect and setup a Twitter instance
-	 *
-	 */
-	private function connect() {
-		\Codebird\Codebird::setConsumerKey($this->consumerKey, $this->consumerSecret);
-		$this->cb = \Codebird\Codebird::getInstance();
-		$this->cb->setToken($this->accessToken, $this->accessSecret);
-	}
+    /**
+     *
+     * Connect and setup a Twitter instance
+     *
+     */
+    private function connect()
+    {
+        \Codebird\Codebird::setConsumerKey($this->consumerKey, $this->consumerSecret);
+        $this->cb = \Codebird\Codebird::getInstance();
+        $this->cb->setToken($this->accessToken, $this->accessSecret);
+    }
 
-	/**
-	 *
-	 * Fetch tweets for a instance and a screen name
-	 *
-	 * @param int $count number of tweets to fetch
-	 * @param boolean $retweet Do we need to display retweets
-	 * @param string $screenName Screen Name for a user timeline
-	 */
-	public function getTweets($count = 1, $retweet = false, $screenName = null) {
-		$this->setScreenName($screenName);
-		$params = array(
-			'screen_name' => $this->screenName,
-			'count' => $count,
-			'include_rts' => $retweet
-		);
-		$tweets = $this->cb->statuses_userTimeline($params);
-		return $tweets;
-	} 
+    /**
+     *
+     * Fetch tweets for a instance and a screen name
+     *
+     * @param int $count number of tweets to fetch
+     * @param boolean $retweet Do we need to display retweets
+     * @param string $screenName Screen Name for a user timeline
+     */
+    public function getTweets($count = 1, $retweet = false, $screenName = null)
+    {
+        $this->setScreenName($screenName);
+        $params = array(
+            'screen_name' => $this->screenName,
+            'count' => $count,
+            'include_rts' => $retweet
+        );
+        $tweets = $this->cb->statuses_userTimeline($params);
+        return $tweets;
+    }
 
-	/**
-	 *
-	 * Send a tweet
-	 *
-	 * @param string $tweet The tweet to send
-	 */
-	public function sendTweet($tweet) {
-		if (is_array($tweet)) {
-			$params = $tweet;
-		} else {
-			$params['status'] = $tweet;
-		}
-		$reply = $this->cb->statuses_update($params);
-		return $reply;
-	}
+    /**
+     *
+     * Send a tweet
+     *
+     * @param string $tweet The tweet to send
+     */
+    public function sendTweet($tweet)
+    {
+        if (is_array($tweet)) {
+            $params = $tweet;
+        } else {
+            $params['status'] = $tweet;
+        }
+        $reply = $this->cb->statuses_update($params);
+        return $reply;
+    }
 
-	/**
-	 *
-	 * Search for something on Twitter
-	 *
-	 * @param string $search The query you want to search for
-	 */
-	public function search($search) {
-		return $this->cb->search_tweets("q=$search", true);
-	}
+    /**
+     *
+     * Search for something on Twitter
+     *
+     * @param string $search The query you want to search for
+     */
+    public function search($search)
+    {
+        return $this->cb->search_tweets("q=$search", true);
+    }
 }
